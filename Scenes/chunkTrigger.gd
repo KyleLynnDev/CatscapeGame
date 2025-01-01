f2d1f11d0a3d2c0221383b3fa711828d8835bda9
Kyle Lynn <lynninlcn@gmail.com>
2025-01-01 10:56:30 -0800

    Adding basic elements of chunk system

---
+++ Scenes/chunkTrigger.gd
@@ -0,0 +1,17 @@
+extends Area3D
+
+@export var chunk_id: String;
+@export var chunk_position: Vector3;
+@export var load_radius: float = 12.0; 
+
+var player: Node = null;
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
+
+
+
+
+func _on_body_exited(body):
+	pass # Replace with function body.
---
+++ Scenes/node.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var loaded_chunks: Dictionary = {};
+
+
+func load_chunk(chunk_id: String, chunk_position: Vector3):
+	
